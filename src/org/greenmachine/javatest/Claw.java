26367d8223fbae422a6427f3c361a49962288fcf
P Deutsch <pete.deutsch@gmail.com>
2015-10-25 13:43:47 -0500

    added isOpen flag

--- src/org/greenmachine/javatest/Claw.java
+++ src/org/greenmachine/javatest/Claw.java
@@ -2,12 +2,14 @@ package org.greenmachine.javatest;
 
 public class Claw {
     private String name;
+	private boolean isOpen;
 
     public Claw(String name) {
     	this.name = name;
     }
 
     public void open() {
+		isOpen = true;
         System.out.println(name + ": claw opening");
     }
 }
